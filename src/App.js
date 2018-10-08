1afabd4850ecad69dedbe2dfaadd463a36520ad9
dragosiordachioaia <xdragosy@gmail.com>
2018-10-08 18:44:08 +0100

    Add initial basic setup for calling the API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import axios from "axios";
+import { ENDPOINTS, REQUEST_PARAMS } from "./constants";
+
+axios.defaults.params = REQUEST_PARAMS;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      genres: null,
+      movies: null,
+      config: null,
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+    this.getImagePath = this.getImagePath.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    axios.get(ENDPOINTS.config).then(({ data }) => {
+      this.setState({ config: data.images });
+    });
+    axios.get(ENDPOINTS.nowPlaying).then(({ data }) => {
+      this.setState({ movies: data.results }, () => {});
+    });
+    axios.get(ENDPOINTS.movieGenres).then(({ data }) => {
+      this.setState({ genres: data.genres });
+    });
+  }
+
+  getImagePath(imageID) {
+    if (!this.state.config) {
+      return null;
+    }
+
+    return `${this.state.config.base_url}${
+      this.state.config.poster_sizes[4]
+    }${imageID}`;
+  }
+
   render() {
+    if (!this.state.config || !this.state.movies || !this.state.genres) {
+      return <p>Loading...</p>;
+    }
+    let posterPath = this.state.movies[0].poster_path;
+    let fullPosterPath = this.getImagePath(posterPath);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={fullPosterPath} />
       </div>
     );
   }
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const API_KEY = "9f0bc88ff0ebfb84f9ac784e09241437";
+export const BASE_URL = "https://api.themoviedb.org/3";
+export const ENDPOINTS = {
+  nowPlaying: `${BASE_URL}/movie/now_playing`,
+  movieGenres: `${BASE_URL}/genre/movie/list`,
+  config: `${BASE_URL}/configuration`,
+};
+export const REQUEST_PARAMS = {
+  api_key: API_KEY,
+};
