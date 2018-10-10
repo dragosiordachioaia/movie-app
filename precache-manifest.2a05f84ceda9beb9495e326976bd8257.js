61e980a89112ad371d878b6c42b7b2f1d6be4a25
dragosiordachioaia <xdragosy@gmail.com>
2018-10-10 12:31:12 +0100

    Updates

---
+++ precache-manifest.2a05f84ceda9beb9495e326976bd8257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/movie-app/static/media/futura_light.98d2f973.ttf"
+  },
+  {
+    "revision": "3ea9695ee3af8a5e17413f22249f361a",
+    "url": "/movie-app/static/media/futura_heavy.3ea9695e.ttf"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "8cd36b749dc482178638",
+    "url": "/movie-app/static/js/main.8cd36b74.chunk.js"
+  },
+  {
+    "revision": "d182e2c6d22c409e2a21",
+    "url": "/movie-app/static/js/1.d182e2c6.chunk.js"
+  },
+  {
+    "revision": "8cd36b749dc482178638",
+    "url": "/movie-app/static/css/main.748217af.chunk.css"
+  },
+  {
+    "revision": "2304cf38d6ed3d98eba97997d2ee2d45",
+    "url": "/movie-app/index.html"
+  }
+];
