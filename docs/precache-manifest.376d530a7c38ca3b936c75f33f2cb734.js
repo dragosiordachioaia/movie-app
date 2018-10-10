9ce434d56e233b1421c53555a25f76a57185c718
dragosiordachioaia <xdragosy@gmail.com>
2018-10-10 12:04:50 +0100

    Add docs folder for github pages

---
+++ docs/precache-manifest.376d530a7c38ca3b936c75f33f2cb734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/static/media/futura_light.98d2f973.ttf"
+  },
+  {
+    "revision": "3ea9695ee3af8a5e17413f22249f361a",
+    "url": "/static/media/futura_heavy.3ea9695e.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "93a9868522e07ef13399",
+    "url": "/static/js/main.93a98685.chunk.js"
+  },
+  {
+    "revision": "d182e2c6d22c409e2a21",
+    "url": "/static/js/1.d182e2c6.chunk.js"
+  },
+  {
+    "revision": "93a9868522e07ef13399",
+    "url": "/static/css/main.d9ba239e.chunk.css"
+  },
+  {
+    "revision": "8b042b94c85354301495e5b1069e3aaa",
+    "url": "/index.html"
+  }
+];
